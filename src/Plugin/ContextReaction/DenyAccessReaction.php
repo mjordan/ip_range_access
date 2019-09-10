7a524e30e16b8e4eb781b703291001a40aeaf2a0
Mark Jordan <mjordan@sfu.ca>
2019-09-10 05:15:25 +0000

    Work on #2.

--- src/Plugin/ContextReaction/DenyAccessReaction.php
+++ src/Plugin/ContextReaction/DenyAccessReaction.php
@@ -4,6 +4,8 @@ namespace Drupal\ip_range_access\Plugin\ContextReaction;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\context\ContextReactionPluginBase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Denies access (returns 403 Access Denied) to user.
@@ -21,6 +23,7 @@ class DenyAccessReaction extends ContextReactionPluginBase {
   public function defaultConfiguration() {
     return parent::defaultConfiguration() + [
       'deny_access' => FALSE,
+      'proxy_prepend_url' => '',
     ];
   }
 
@@ -37,7 +40,13 @@ class DenyAccessReaction extends ContextReactionPluginBase {
   public function execute() {
     $config = $this->getConfiguration();
     if ($config['deny_access']) {
-      throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
+      throw new AccessDeniedHttpException();
+    }
+    if (strlen($config['proxy_prepend_url'])) {
+      $current_url = $host = \Drupal::request()->getSchemeAndHttpHost() . \Drupal::service('path.current')->getPath();
+      $redirect_url = $config['proxy_prepend_url'] . $current_url;
+      $response = new RedirectResponse($redirect_url);
+      $response->send();
     }
   }
 
@@ -52,9 +61,15 @@ class DenyAccessReaction extends ContextReactionPluginBase {
       '#description' => $this->t('Check this box to return a 403 Access Denied response to the user.'),
       '#default_value' => isset($config['deny_access']) ? $config['deny_access'] : FALSE,
     ];
+    $form['proxy_prepend_url'] = [
+      '#type' => 'textfield',
+      '#title' => t('Proxy URL'),
+      '#default_value' => $this->configuration['proxy_prepend_url'],
+      '#maxlength' => 256,
+      '#description' => t('URL to redirect users to, e.g., an Ezproxy login URL. Leave blank to not redirect user.'),
+    ];
     return $form;
   }
-
 
   /**
    * {@inheritdoc}
@@ -62,6 +77,7 @@ class DenyAccessReaction extends ContextReactionPluginBase {
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     $this->setConfiguration([
       'deny_access' => $form_state->getValue('deny_access'),
+      'proxy_prepend_url' => $form_state->getValue('proxy_prepend_url'),
     ]);
   }
 
